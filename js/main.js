ab73510b6bf03351e307e51fbfd8ff6d8882e750
Arthur Silva Dantas <arthur.dantas@estudante.ifms.edu.br>
2023-09-23 02:37:11 -0300

    Index e Biblioteca

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
+}
+
+/* Testimonials carousel slider swiper */
+var swiper = new Swiper(".testimonials", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
